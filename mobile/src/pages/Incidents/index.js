27b18e8ac6db9c65905b6d4c791e89581a2a469a
Ygor Azambuja <ygorazambuja@gmail.com>
2020-03-26 11:24:21 -0400

    começando mobile

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import logoImg from '../../assets/logo.png'
+
+export default function Incidents() {
+  return (
+    <View>
+      <Text>Oi</Text>
+    </View>
+  );
+}
